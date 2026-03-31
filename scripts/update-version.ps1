2b6ab0da3387ace9978262a30f1401e57ab98a56
Matthew Tucciarone <mjtucciarone@protonmail.com>
2026-03-31 20:36:21 +0700

    feat: fix releases

--- scripts/update-version.ps1
+++ scripts/update-version.ps1
@@ -1,20 +1,22 @@
+$ErrorActionPreference = "Stop"
+
 param (
   [string]$version
 )
 
 Write-Host "🔄 Updating Cargo.toml to version $version"
 
-# Replace version in Cargo.toml
-(Get-Content Cargo.toml) -replace 'version = ".*"', "version = `"$version`"" |
+(Get-Content Cargo.toml) `
+  -replace '^version = ".*"', "version = `"$version`"" |
   Set-Content Cargo.toml
 
-# Verify
 Write-Host "✅ Updated Cargo.toml:"
-Select-String 'version =' Cargo.toml
+Select-String '^version =' Cargo.toml
 
-# Commit the change
 git config user.name "github-actions"
 git config user.email "github-actions@github.com"
 
 git add Cargo.toml
 git commit -m "chore(release): $version [skip ci]"
+
+git push origin HEAD:main
